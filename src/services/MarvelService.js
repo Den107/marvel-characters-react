726c6c1def60b0b55678360768c6a8fe7db6e1ba
Denis Kravchuk <deniskrv@gmail.com>
2022-06-09 22:19:18 +0300

    оптимизировал запросы к api и state в компоненте random char

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -3,7 +3,7 @@ class MarvelService {
     static API_KEY = '2a18360a8c31c765a4152e81f54bdfdc'
     static BASE_URL = 'https://gateway.marvel.com:443/v1/public/'
 
-   static getResource = async (url) => {
+    static getResource = async (url) => {
         let res = await fetch(url)
 
         if (!res.ok) {
@@ -13,12 +13,24 @@ class MarvelService {
         return await res.json()
     }
 
-    getAllCharacters = () => {
-        return MarvelService.getResource(`${MarvelService.BASE_URL}characters?limit=9&offset=210&apikey=${MarvelService.API_KEY}`)
+    getAllCharacters = async () => {
+        const res = await MarvelService.getResource(`${MarvelService.BASE_URL}characters?limit=9&offset=210&apikey=${MarvelService.API_KEY}`)
+        return res.data.results.map(MarvelService._transformCharacter)
     }
 
-    getCharacter = (id) => {
-        return MarvelService.getResource(`${MarvelService.BASE_URL}characters/${id}?apikey=${MarvelService.API_KEY}`)
+    getCharacter = async (id) => {
+        const res = await MarvelService.getResource(`${MarvelService.BASE_URL}characters/${id}?apikey=${MarvelService.API_KEY}`)
+        return MarvelService._transformCharacter(res.data.results[0])
+    }
+
+    static _transformCharacter = char => {
+        return {
+            name: char.name,
+            description: char.description || 'Description not found...',
+            thumbnail: `${char.thumbnail.path}.${char.thumbnail.extension}`,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
     }
 }
 

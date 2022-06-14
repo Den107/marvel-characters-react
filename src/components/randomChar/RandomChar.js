bbf0a2222d4eac2055aa7953fd39f7e1fa527f59
Denis Kravchuk <deniskrv@gmail.com>
2022-06-14 17:33:16 +0300

    для компонента рандомного персонажа добавил хук componentDidMount и перенес в него updateChar

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -8,10 +8,7 @@ import Spinner from "../spinner/Spinner";
 import ErrorMessage from "../errorMessage/ErrorMessage";
 
 class RandomChar extends Component {
-    constructor(props) {
-        super(props);
-        this.updateChar()
-    }
+
 
     state = {
         char: {},
@@ -20,6 +17,10 @@ class RandomChar extends Component {
     }
 
     marvelService = new MarvelService()
+
+    componentDidMount() {
+        this.updateChar()
+    }
 
     onCharLoaded = char => {
         this.setState({char, loading: false})
